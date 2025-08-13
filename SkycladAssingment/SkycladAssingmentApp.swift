a86746fa4e9756b816eabee445a40f443f7515d8
dramaticking99 <cksanwariya6@gmail.com>
2025-08-13 21:05:09 +0530

    Initial Commit

---
+++ SkycladAssingment/SkycladAssingmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkycladAssingmentApp.swift
+//  SkycladAssingment
+//
+//  Created by Chetan Sanwariya on 13/08/25.
+//
+
+import SwiftUI
+
+@main
+struct SkycladAssingmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
